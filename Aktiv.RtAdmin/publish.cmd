de9d7f2537c47fbcab0b6fd8695af1b0311011ef
Pavel Khrulev <pahan4igg@gmail.com>
2019-06-18 00:48:27 +0300

    Add publish profiles

---
+++ Aktiv.RtAdmin/publish.cmd
@@ -0,0 +1,4 @@
+dotnet publish /p:PublishProfile=.\Properties\PublishProfiles\win64.pubxml
+dotnet publish /p:PublishProfile=.\Properties\PublishProfiles\win32.pubxml
+dotnet publish /p:PublishProfile=.\Properties\PublishProfiles\osx64.pubxml
+dotnet publish /p:PublishProfile=.\Properties\PublishProfiles\linux64.pubxml
