098c18f1ec303deff0f7d1c7436a00a024b14a3d
Pavel Khrulev <pahan4igg@gmail.com>
2019-06-27 00:43:05 +0300

    Add native libraries nuget

--- Aktiv.RtAdmin/Startup.cs
+++ Aktiv.RtAdmin/Startup.cs
@@ -4,15 +4,14 @@ using Net.Pkcs11Interop.HighLevelAPI;
 using Serilog;
 using System;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Text;
-using RutokenPkcs11Interop;
 
 namespace Aktiv.RtAdmin
 {
     public static class Startup
     {
 #if DEBUG
-        private static readonly string pkcs11NativeLibraryName = Settings.RutokenEcpDllDefaultPath;
 #else
         private const string pkcs11NativeLibraryName = "pkcs11NativeLib.dll";
 #endif
@@ -47,13 +46,35 @@ namespace Aktiv.RtAdmin
 
             return new ServiceCollection()
                 .AddLogging(loggingBuilder => loggingBuilder.AddSerilog(dispose: true))
-                .AddSingleton(s => new Pkcs11(Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), pkcs11NativeLibraryName), AppType.MultiThreaded))
+                .AddSingleton(s => new Pkcs11(Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), GetNativeLibraryName()), AppType.MultiThreaded))
                 .AddSingleton<PinsStore>()
                 .AddSingleton<VolumeOwnersStore>()
                 .AddTransient<RutokenCore>()
                 .AddTransient<TokenParams>()
                 .AddTransient<CommandHandlerBuilder>()
                 .BuildServiceProvider();
+        }
+
+        private static string GetNativeLibraryName()
+        {
+#if DEBUG
+            return Settings.RutokenEcpDllDefaultPath;
+#else
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                return "rtpkcs11ecp.dll";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                return "librtpkcs11ecp.so";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                return "librtpkcs11ecp.dylib";
+            }
+
+            throw new InvalidOperationException("Incorrect OS version");
+#endif
         }
     }
 }
