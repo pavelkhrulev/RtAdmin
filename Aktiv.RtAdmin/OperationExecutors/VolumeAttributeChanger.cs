fff9f8bde89f0f7d0b6fe41dbbc9a44fbdc9d953
Pavel Khrulev <pahan4igg@gmail.com>
2019-07-23 01:01:52 +0300

    Incorrent PIN handler

--- Aktiv.RtAdmin/OperationExecutors/VolumeAttributeChanger.cs
+++ Aktiv.RtAdmin/OperationExecutors/VolumeAttributeChanger.cs
@@ -1,4 +1,6 @@
-﻿using Net.Pkcs11Interop.HighLevelAPI;
+﻿using Aktiv.RtAdmin.Properties;
+using Net.Pkcs11Interop.Common;
+using Net.Pkcs11Interop.HighLevelAPI;
 using RutokenPkcs11Interop.HighLevelAPI;
 
 namespace Aktiv.RtAdmin
@@ -8,12 +10,19 @@ namespace Aktiv.RtAdmin
         public static void Change(Slot slot, 
             ChangeVolumeAttributesParams volumeAttributes)
         {
-            slot.ChangeVolumeAttributes(
-                volumeAttributes.VolumeOwner,
-                volumeAttributes.OwnerPin,
-                (uint)volumeAttributes.VolumeId,
-                volumeAttributes.AccessMode,
-                volumeAttributes.Permanent);
+            try
+            {
+                slot.ChangeVolumeAttributes(
+                    volumeAttributes.VolumeOwner,
+                    volumeAttributes.OwnerPin,
+                    (uint)volumeAttributes.VolumeId,
+                    volumeAttributes.AccessMode,
+                    volumeAttributes.Permanent);
+            }
+            catch (Pkcs11Exception ex) when (ex.RV == CKR.CKR_PIN_INCORRECT)
+            {
+                throw new CKRException(ex.RV, Resources.IncorrectPin);
+            }
         }
     }
 }
