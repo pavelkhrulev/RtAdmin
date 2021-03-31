893d4879a983366a3f8b9ae9c32431069314f4b9
lo1ol <mkh199740@mail.ru>
2021-03-31 13:55:06 +0300

    update pkcs11lib and add building for arm platforms

--- publish.bat
+++ publish.bat
@@ -1,3 +1,5 @@
 dotnet publish -c Release  -r win-x86 --self-contained -o publish/win-x86
 dotnet publish -c Release  -r osx-x64 --self-contained -o publish/osx-x64
 dotnet publish -c Release  -r linux-x64 --self-contained -o publish/linux-x64
+dotnet publish -c Release  -r linux-arm64 --self-contained -o publish/linux-arm64
+dotnet publish -c Release  -r linux-arm --self-contained -o publish/linux-armv32
